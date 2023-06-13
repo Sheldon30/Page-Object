0cd3d8c91456349a7e48e07d7e4233f1f3b60611
Sheldon <aneln700@gmail.com>
2023-06-13 15:23:29 +0300

    Initial

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,16 +1,17 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPageV1;
 import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
 
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
+    void shouldTransferMoneyBetweenOwnCards() {
       open("http://localhost:9999");
       var loginPage = new LoginPageV1();
 //    var loginPage = open("http://localhost:9999", LoginPageV1.class);
@@ -18,22 +19,16 @@ class MoneyTransferTest {
       var verificationPage = loginPage.validLogin(authInfo);
       var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
       verificationPage.validVerify(verificationCode);
+
+
     }
+
 
   @Test
   void shouldTransferMoneyBetweenOwnCardsV2() {
     open("http://localhost:9999");
     var loginPage = new LoginPageV2();
 //    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
-
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
     var authInfo = DataHelper.getAuthInfo();
     var verificationPage = loginPage.validLogin(authInfo);
     var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
