4bd434d47c6499052267e8ceac93c94f0e6f5969
Sheldon <aneln700@gmail.com>
2023-06-18 15:26:12 +0300

    Initial

--- src/test/java/ru/netology/web/page/TransferPage.java
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -2,15 +2,36 @@ package ru.netology.web.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class TransferPage {
-    public TransferPage Card1(){
-        $$("[data-test-id='action-deposit']").get(1).click();
-        return new TransferPage();
+    private SelenideElement amount = $("[data-test-id='amount'] input");
+    private SelenideElement fromAmount = $("[data-test-id='from'] input");
+    private SelenideElement transferHead = $(byText("Пополнение карты"));
+
+    private SelenideElement amountButton = $("[data-test-id='action-transfer']");
+    private SelenideElement errorMessage = $("[data-test-id='error-notification']");
+    public TransferPage(){
+        transferHead.shouldBe(Condition.visible);
+    }
+    public void makeTransfer (String transferAmount, DataHelper.CardInfo cardInfo){
+        amount.setValue(transferAmount);
+        fromAmount.setValue(cardInfo.getCardNumber());
+        amountButton.click();
+
+    }
+    public DashboardPage makeValidAmount(String transferAmount, DataHelper.CardInfo cardInfo) {
+        makeTransfer(transferAmount, cardInfo);
+        return new DashboardPage();
     }
 
+    public void ErrorMessageFind(String expectedText){
+        errorMessage.shouldHave(Condition.exactText(expectedText), Duration.ofSeconds(10)).shouldBe(Condition.visible);
 
+    }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,38 +1,35 @@
 package ru.netology.web.test;
 
-import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPageV1;
-import ru.netology.web.page.LoginPageV2;
 
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyBetweenOwnCards() {
+  DashboardPage dashboardPage;
+
+
+    @BeforeEach
+    public void setUp() {
       open("http://localhost:9999");
       var loginPage = new LoginPageV1();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
       var authInfo = DataHelper.getAuthInfo();
       var verificationPage = loginPage.validLogin(authInfo);
       var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-      verificationPage.validVerify(verificationCode);
-
+      dashboardPage = verificationPage.validVerify(verificationCode);
 
     }
 
 
   @Test
   void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
+    var firstCard = DataHelper.getFirstCard();
+    var secondCard = DataHelper.getSecondCard();
+    var firstCardBalance = DashboardPage.
+
   }
 }
 
